828f25a93bb6ae53a255d60f210db6246f9cd1ce
Lee Azzarello <lee@rockingtiger.com>
2012-03-02 13:43:29 -0800

    attributes and templates for OSTN compliance

---
+++ cookbooks/freeswitch/attributes/defaults.rb
@@ -0,0 +1,4 @@
+default[:freeswitch][:git_uri] = "git://git.freeswitch.org/freeswitch.git"
+default[:freeswitch][:inbound_proxy_media] = true
+default[:freeswitch][:inbound_bypass_media] = false
+default[:freeswitch][:sip_tls_version] = "sslv23"
