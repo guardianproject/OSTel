a04e9a6428eec3cfccbc5e431b6359a7e36ab009
Lee Azzarello <lee@rockingtiger.com>
2012-03-02 13:47:41 -0800

    default recipe

--- cookbooks/freeswitch/recipes/default.rb
+++ cookbooks/freeswitch/recipes/default.rb
@@ -1,18 +1,71 @@
+## Cookbook Name:: freeswitch
+## Recipe:: default
+##
+## Copyright 2012, "Twelve Tone Software" <lee@twelvetone.info>
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
 #
-# Cookbook Name:: freeswitch
-# Recipe:: default
-#
-# Copyright 2012, Example Com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+## Freeswitch cookbook
+## ZRTP media pass-through proxy mode
+
+## Build requirements
+package "autoconf" 
+package "automake"
+package "g++"
+package "git-core"
+package "libjpeg62-dev"
+package "libncurses5-dev"
+package "libtool"
+package "make"
+package "python-dev"
+package "gawk"
+package "pkg-config"
+package "gnutls-bin"
+
+# get source
+execute "git_clone" do
+  command "git clone #{node[:freeswitch][:git_uri]}"
+  cwd "/usr/local/src"
+  creates "/usr/local/src/freeswitch"
+end
+
+# compile source
+script "compile_freeswitch" do
+  shell "/bin/bash"
+  script <<<EOF
+  cd /usr/local/src/freeswitch
+  ./bootstrap.sh
+  ./configure
+  make
+EOF
+end
+
+# install software
+execute "install_freeswitch" do
+  cwd "/usr/local/src/freeswitch"
+  command "make install"
+end
+
+# set global variables
+template "/usr/local/freeswitch/conf/vars.xml" do
+  source "vars.xml.erb"
+  mode 0644
+end
+
+# set SIP security attributes
+template "/usr/local/freeswitch/conf/sip_profiles/internal.xml" do
+  source "internal.xml.erb"
+  mode 0644
+end
