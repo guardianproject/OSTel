a644599261e27211f154ab79d3469a8e5ebc42fb
Lee Azzarello <lee@rockingtiger.com>
2012-03-29 18:03:03 -0400

    commit bare webapp cookbook

---
+++ cookbooks/ostn/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: ostn
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+gem_package "unicorn"
+gem_package "sinatra"
