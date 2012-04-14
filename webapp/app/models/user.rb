98d831ec0a3dba2991dc9fac458cbb59ba7dd58a
Zach Schwartz <zischwartz@gmail.com>
2012-04-14 17:13:08 -0400

    added email validation

--- webapp/app/models/user.rb
+++ webapp/app/models/user.rb
@@ -1,3 +1,14 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
+
 class User < ActiveRecord::Base
   attr_accessible :email, :login, :name, :password, :pgpkey
+  validates :email, :presence => true, :email => true
+
 end
