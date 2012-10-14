11d7e0a05af0a3ceb3768929a3db0d187d8a7a4c
Lee Azzarello <lee@rockingtiger.com>
2012-10-14 17:46:12 -0400

    that stupid true bug again?

--- cookbooks/freeswitch/recipes/default.rb
+++ cookbooks/freeswitch/recipes/default.rb
@@ -85,7 +85,7 @@ end
 
 # create non-root user
 user node[:freeswitch][:user] do
-  system "true"
+  system true
   shell "/bin/bash"
   home node[:freeswitch][:homedir]
   gid node[:freeswitch][:group]
