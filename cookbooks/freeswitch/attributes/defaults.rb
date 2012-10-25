c96e527b5fdb6dfacee08e820dd75c2c48dc654a
Lee Azzarello <lee@rockingtiger.com>
2012-10-25 12:21:44 -0400

    stupid ZRTP changes broke ZRTP! WTF!? This commit is the last known good version.

--- cookbooks/freeswitch/attributes/defaults.rb
+++ cookbooks/freeswitch/attributes/defaults.rb
@@ -1,5 +1,5 @@
 default[:freeswitch][:git_uri] = "git://git.freeswitch.org/freeswitch.git"
-default[:freeswitch][:release_tag] = "v1.2.3"
+default[:freeswitch][:release_tag] = "2ab1605a8887adc62be1b75f6ef67af87ff080de"
 default[:freeswitch][:inbound_proxy_media] = "true"
 default[:freeswitch][:inbound_bypass_media] = "false"
 default[:freeswitch][:sip_tls_version] = "sslv23"
