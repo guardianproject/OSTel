a75734b213807d9a5c85e75d02a8b466726399b7
Lee Azzarello <lee@rockingtiger.com>
2013-04-05 10:34:55 -0400

    set up routes and templates for static controllers

--- webapp/app/controllers/home_controller.rb
+++ webapp/app/controllers/home_controller.rb
@@ -4,6 +4,12 @@ class HomeController < ApplicationController
   def index
   end
 
+  def about
+  end
+
+  def faq
+  end
+
   def token
   end
 end
--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -2,11 +2,14 @@ DeviseExample::Application.routes.draw do
 	#these symbols map to models
   devise_for :users, :admin
 
-	#these symbols map to controllers
-  resources :home, :only => :index
+  #these symbols map to controllers
+  match '/about' => 'home#about', :as => 'about'
+  match '/faq' => 'home#faq', :as => 'faq'
+  root :to => 'home#index'
+
+  # no admins yet. Devise makes them by default though.
   resources :admins, :only => :index
 
-  root :to => 'home#index'
 
   match '/token' => 'home#token', :as => :token
 end
