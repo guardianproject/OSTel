1da9fa825a908a1bbfaafb0c3886b3c15149cb68
Lee Azzarello <lee@rockingtiger.com>
2013-04-15 00:04:23 -0400

    generate kamailio ha1 hash based on user data

--- webapp/app/models/user.rb
+++ webapp/app/models/user.rb
@@ -3,5 +3,12 @@ class User < ActiveRecord::Base
          :rememberable, :registerable, :timeoutable, :validatable,
          :token_authenticatable
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :ha1, :ha1b, :username
+  before_create :generate_sip_hash
+
+  protected
+  def generate_sip_hash
+    self.ha1 = Digest::MD5.hexdigest("#{self.username}:#{self.password}:#{Devise::Application.config.domain}")
+    self.ha1b = Digest::MD5.hexdigest("#{self.username}@#{Devise::Application.config.domain}:#{self.password}:#{Devise::Application.config.domain}")
+  end
 end
