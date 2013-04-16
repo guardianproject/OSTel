420bd4442769a35c61a9ecb9997117c10c64447d
Lee Azzarello <lee@rockingtiger.com>
2013-04-16 18:17:03 -0400

    kamailio uses interesting conventions.

--- webapp/db/migrate/20130416212105_create_kamailio_version_table.rb
+++ webapp/db/migrate/20130416212105_create_kamailio_version_table.rb
@@ -6,12 +6,18 @@ class CreateKamailioVersionTable < ActiveRecord::Migration
     execute "CREATE TABLE version (table_name VARCHAR(32) NOT NULL, table_version INTEGER DEFAULT 0 NOT NULL)"
     # kamailio also uses some internal versioning system in the database. The
     # version strings for each table are set in SQL files distributed upstream.
-    # I have copied them here...this does not scale.
+    # I have copied the relevant SQL here...this does not scale.
     # 
     # I have used the table_name of 'users' here, which corresponds to the Rails
-    # model. I would like to avoid using views, though it might be a good idea
+    # model. I would like to avoid using views to stick with the default
+    # kamailio table names even through the documentation claims they can be
+    # overridden, though it might be a good idea
     # to eventually migrate the whole kamailio database to postgres.
     execute "INSERT INTO version (table_name, table_version) VALUES ('users','6')"
+    # Kamailio seems to have the table name "subscriber" as immutable string
+    # somewhere in the C source code of the db module. I'm not fucking with
+    # that. Faking it till I make it.
+    execute "INSERT INTO version (table_name, table_version) VALUES ('subscriber','6')"
   end
 
   def self.down
