24ed1748554bc32ad9f7010fa25b3ae01860aa39
Lee Azzarello <lee@rockingtiger.com>
2013-04-17 18:29:11 -0400

    first steps to have a multi-stage transaction for user creation. This is a touch one.

--- webapp/app/models/user.rb
+++ webapp/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :ha1, :ha1b, :sip_username, :domain
   before_save :generate_sip_hash
-  validates :sip_username, :uniqueness => true
+  validates :sip_username, :uniqueness => true, :presence => true
 
   def create_suggestions
     desired_username = self.email.split("@").first
