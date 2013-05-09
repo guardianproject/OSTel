5f152d69ef672d13a5096b0b088551b7d7a7e99a
Lee Azzarello <lee@rockingtiger.com>
2013-05-09 15:08:47 -0400

    add repo and packages to default recipe

--- cookbooks/kamailio/recipes/default.rb
+++ cookbooks/kamailio/recipes/default.rb
@@ -2,7 +2,30 @@
 # Cookbook Name:: kamailio
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Lee Azzarello <lee@guardianproject.info>
 #
-# All rights reserved - Do Not Redistribute
+# GPLv3
 #
+
+# Add Kamailio package source to the system's database
+execute "apt-get-update" do
+  command "apt-get update"
+  action :nothing
+end
+
+template "/etc/apt/sources.list.d/kamailio.list" do
+  source "kamailio.list.erb"
+  creates "/etc/apt/sources.list.d/kamailio.list"
+  notifies :run, "execute[apt-get-update]", :immediately
+end
+
+# install kamailio and friends
+
+package "kamailio"
+package "kamailio-tls-modules" #essential
+package "kamailio-utils-modules" # some non-SIP related utility functions
+package "kamailio-presence-modules" # might as well support SIP presence
+package "kamailio-xmpp-modules" # support an XMPP gateway
+package "rtpproxy" # default configuration is fine
+
+# there are two configuration files required for kam
