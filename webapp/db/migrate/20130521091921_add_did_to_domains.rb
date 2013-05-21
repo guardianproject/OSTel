4ac80f10ab1eeaa06401e60e20046c7bc0edbd15
Lee Azzarello <lee@rockingtiger.com>
2013-05-21 05:49:50 -0400

    update domains table to kam 4.0

---
+++ webapp/db/migrate/20130521091921_add_did_to_domains.rb
@@ -0,0 +1,11 @@
+class AddDidToDomains < ActiveRecord::Migration
+  def self.up
+    add_column :domains, :did
+    execute "UPDATE version set table_version=2 where table_name = 'domain'"
+  end
+
+  def self.down
+    remove_column :domains, :did
+    execute "UPDATE version set table_version=1 where table_name = 'domain'"
+  end
+end
