e8b2a5e644375a65fad93dbcc5a7d6545f1a6544
Lee Azzarello <lee@rockingtiger.com>
2013-06-03 11:42:43 -0400

    first shot at changing user password for signed in user

--- webapp/app/controllers/users_controller.rb
+++ webapp/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :only => [:link, :show, :update, :delete, :create]
+  before_filter :authenticate_user!, :only => [:link, :show, :update, :delete, :create, :edit]
 
   def suggest
     @user = User.new(params[:user])
@@ -22,10 +22,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-
-  end
-
   def update
     @user = User.find(params[:id])
     @user.update_attributes(params[:user])
@@ -34,9 +30,5 @@ class UsersController < ApplicationController
     else
       redirect_to users_suggest_path, :flash => { :user_unavailable => "The username #{params[:user][:sip_username]} is not available"}
     end
-  end
-
-  def delete
-
   end
 end
