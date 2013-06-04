04583be97b7c67db5e9dabfaaddd05f0e6808c70
Lee Azzarello <lee@rockingtiger.com>
2013-06-04 15:32:28 -0400

    use the signed in user's id, not the one passed from the view params

--- webapp/app/controllers/users_controller.rb
+++ webapp/app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     @user.update_attributes(params[:user])
     if (@user.save)
       redirect_to @user
