f8d00d8a52a46e7b5215fdc674a5beae5d119043
Lee Azzarello <lee@rockingtiger.com>
2013-07-25 18:48:57 -0400

    TIME FOR POSTGRES! Brace yourself for the next commit.

--- cookbooks/kamailio/recipes/postgresql_mod.rb
+++ cookbooks/kamailio/recipes/postgresql_mod.rb
@@ -7,5 +7,15 @@
 # GPLv3
 #
 #
+# assume postgres is already installed
 package "kamailio-postgres-modules"
+
+# now we have the DDL on disk but no database yet. make one
+
+execute "create-database" do
+  command "createdb #{node[:kamailio][:dbname]}"
+  creates "file on disk"
+  user "postgres"
+end
+
 
