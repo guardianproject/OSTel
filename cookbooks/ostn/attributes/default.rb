54305aece331a7db7c5036242991338190824848
Lee Azzarello <lee@rockingtiger.com>
2013-09-17 17:11:07 -0400

    build the front end cookbook a little

--- cookbooks/ostn/attributes/default.rb
+++ cookbooks/ostn/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:ostn][:git_uri] = "git://github.com/guardianproject/OSTel.git"
+default[:ostn][:webapp_dir] = "/usr/local/freeswitch/webapp"
--- cookbooks/ostn/recipes/default.rb
+++ cookbooks/ostn/recipes/default.rb
@@ -2,19 +2,26 @@
 # Cookbook Name:: ostn
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Twelve Tone Software
 #
-# All rights reserved - Do Not Redistribute
+# GPL version 2 placeholder
+# https://www.gnu.org/licenses/gpl2.txt
 #
 
 package "readline6-dev"
-gem_package "unicorn"
-gem_package "sinatra"
+gem_package "bundler"
 
-#
+# the freeswitch cookbook must be run before this resource
 # get source
 execute "git_clone" do
   command "git clone #{node[:ostn][:git_uri]}"
-  cwd "/usr/local/freeswitch"
+  cwd "/usr/local/src"
   creates "/usr/local/src/OSTel"
+end
+
+# copy source directory without git repo to /usr/local/freeswitch
+
+execute "bundle_install" do
+	command "bundle install"
+	cwd "/usr/local/freeswitch/webapp"
 end
